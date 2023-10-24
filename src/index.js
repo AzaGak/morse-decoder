6804aa8105bd77ffc510f3f926bb01a5ae486016
azat <eric@example.com>
2023-10-24 14:12:57 +0500

    feat: complete morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,16 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = "";
+    for (let i = 0; i < expr.length; i += 10) {
+        const chunk = expr.slice(i, i + 10);
+        const morse = chunk === "**********" ? " " : chunk
+            .match(/(10)|(11)|(\*{10})/g)
+            .map(code => code === "10" ? "." : code === "11" ? "-" : "")
+            .join("");
+        result += morse === " " ? morse : MORSE_TABLE[morse];
+    }
+    return result;
 }
 
 module.exports = {
